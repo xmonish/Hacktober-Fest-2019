e706099fb5c22b3164e45da7245bc311e6b4c3d7
Monish Singh <36487698+xmonish@users.noreply.github.com>
2019-10-25 03:09:15 +0530

    Added support to the floating numbers.

--- Python/quadratic.py
+++ Python/quadratic.py
@@ -1,9 +1,8 @@
 
 import math
-a = int(input("coefficient a:"))
-b = int(input("coefficient b:"))
-c = int(input("coefficient c:"))
+a = float(input("coefficient a:"))
+b = float(input("coefficient b:"))
+c = float(input("coefficient c:"))
 posroot = (-b + (math.sqrt((b*b) - (4*a*c))))/2*a
 negroot = (-b - (math.sqrt((b*b) - (4*a*c))))/2*a
-print(posroot)
-print(negroot)
+print ("The roots are : "),print(posroot), ",", print(negroot)
